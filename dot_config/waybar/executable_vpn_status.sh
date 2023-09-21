f6012a76b0ccdfb83ebe9e159ba2b2f5f879bf31
BbqGamer <korba.adam@gmail.com>
2023-09-22 00:45:02 +0200

    Reformat waybar configuration (remove brightness)

--- dot_config/waybar/executable_vpn_status.sh
+++ dot_config/waybar/executable_vpn_status.sh
@@ -6,5 +6,5 @@ if [[ "$state" == "Connected" ]]; then
   REGION=$(piactl get region)
   echo -n '{"text": "'$REGION'", "class": "connected"}'
 else
-  echo -n '{"text": "Disconnected", "class": "disconnected"}'
+  echo -n '{"text": "VPN off", "class": "disconnected"}'
 fi
