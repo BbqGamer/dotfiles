e077403b97c4489d223f7bab91ff0f01c652cd58
BbqGamer <korba.adam@gmail.com>
2023-10-24 21:11:49 +0200

    Add nvim config

---
+++ home/dot_config/nvim/lua/adam/init.lua
@@ -0,0 +1,2 @@
+require("adam.remap")
+require("adam.set")
---
+++ home/dot_config/nvim/lua/adam/packer.lua
@@ -0,0 +1,48 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'shaunsingh/nord.nvim',
+	  config = function()
+		  vim.cmd('colorscheme nord')
+	  end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+  }
+
+  use('jpalardy/vim-slime')
+
+  use('christoomey/vim-tmux-navigator')
+
+  use('github/copilot.vim')
+end)
