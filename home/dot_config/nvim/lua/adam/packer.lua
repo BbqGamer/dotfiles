04355db53cb78ed19ec732ea85701c6e4a65b2d6
BbqGamer <korba.adam@gmail.com>
2023-12-24 14:46:06 +0100

    Add vim commentary plugin

--- home/dot_config/nvim/lua/adam/packer.lua
+++ home/dot_config/nvim/lua/adam/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('tpope/vim-commentary')
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
